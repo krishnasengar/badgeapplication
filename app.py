0ad1faaa80aae48650090e7c58e8f7def040fb08
Krishna Sengar <81618299+professorluigi@users.noreply.github.com>
2023-04-04 23:13:24 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,17 +7,22 @@ app = Flask(__name__)
 @app.route('/spider-web-graph')
 def spider_web_graph():
     # Get query parameters
-    categories = request.args.get('categories', '').split(',')
-    values = [int(v) for v in request.args.get('values', '').split(',')]
+    categories = request.args.get('categories', 'Category A,Category B,Category C,Category D,Category E').split(',')
+    values = [int(v) for v in request.args.get('values', '5,3,7,2,6').split(',')]
     width = int(request.args.get('width', 500))
     height = int(request.args.get('height', 500))
     scale = int(request.args.get('scale', 2))
+    category_text_size = int(request.args.get('category_text_size', 14))
+    value_text_size = int(request.args.get('value_text_size', 14))
 
     # Create the chart object
     trace = go.Scatterpolar(
         r=values,
         theta=categories,
-        fill='toself'
+        fill='toself',
+        text=values,
+        textposition='middle center',
+        textfont_size=value_text_size
     )
 
     # Create the chart layout
@@ -26,6 +31,10 @@ def spider_web_graph():
             radialaxis=dict(
                 visible=True,
                 range=[0, 10]
+            ),
+            angularaxis=dict(
+                showticklabels=True,
+                tickfont=dict(size=category_text_size)
             )
         ),
         showlegend=False,
